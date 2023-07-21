f54744c8a051d3cabf880f61f30749f0cd8b032c
ikromov934 <ikromovking934@gmail.com>
2023-07-21 17:52:55 +0300

    new file

---
+++ main.js
@@ -0,0 +1,27 @@
+var a = 10;
+var b = 5;
+var result = a + b;
+console.log(result);
+
+var a = 25;
+var b = 15;
+var result = a - b;
+console.log(result);
+
+var a = 10;
+var b = 5;
+var c = a - b;
+var d = 7;
+var result = c * d;
+console.log(result);
+
+var a = 10;
+var b = 3;
+var result = a % b;
+console.log(result);
+
+
+
+var a = 2;
+var result = a ** 10;
+console.log(result)
